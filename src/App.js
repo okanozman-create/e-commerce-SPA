ba398c5873b5671022a632dec3bda893d73fbc99
okaozman-create <okanozman88@gmail.com>
2024-03-02 00:31:45 +0300

    İnstalled tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<div>e-commerce-SPA-reactJS</div>
+       <header className="App-header"> 
+       <h1>
+          E-commerce-SPA-reactJS
+        </h1>
+        <h2 className="text-yellow-500 text-5xl">OKAN</h2>
       </header>
     </div>
   );
